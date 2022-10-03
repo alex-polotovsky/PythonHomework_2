5022ac930ea4ccac8efced47aa47e9d0e136408d
alexandr polotovsky <apolotovsky@gmail.com>
2022-10-03 17:17:20 +0300

    edit float_number.py

--- float_number_sum.py
+++ float_number_sum.py
@@ -15,4 +15,14 @@ def get_summ(number):
     return summ
 
 
+def get_summ_2(number):
+    num = str(number)
+    summ = 0
+    for i in num:
+        if i.isdigit():
+            summ += int(i)
+    return summ        
+
+
 print('Сумма цифр числа: ', get_summ(0.56))
+print('Сумма цифр числа: ', get_summ_2(0.56))
